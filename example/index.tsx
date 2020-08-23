de7ba7894126acfdd65aea6cbd4acc927fe592d2
Sidney Alcantara <sidney@sidney.me>
2020-08-23 13:43:04 +1000

    update example

--- example/index.tsx
+++ example/index.tsx
@@ -1,6 +1,8 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import * as yup from 'yup';
+
 import Form from '../src/Form';
 
 const App = () => {
@@ -14,6 +16,21 @@ const App = () => {
           // disabled: true,
           defaultValue: 'something',
         },
+        (values: any) =>
+          values.desc !== 'something'
+            ? {
+                type: 'text',
+                fieldVariant: 'url',
+                name: 'link',
+                label: 'Link',
+                defaultValue: 'https://',
+                validation: yup
+                  .string()
+                  .url('Must be a valid URL')
+                  .matches(/^https?:\/\/.*/, 'Must begin with https://')
+                  .required('Required'),
+              }
+            : null,
       ]}
       onSubmit={data => console.log(data)}
     />
