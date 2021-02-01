fc0840cdb893e70cdcbd1cc780766bf993d5067f
Bobby <bofei@mail.com>
2021-02-01 18:04:41 +1100

    fix text field issue

--- src/Fields/TextField.tsx
+++ src/Fields/TextField.tsx
@@ -69,6 +69,7 @@ export default function TextField({
           margin="none"
           error={!!errorMessage}
           helperText={errorMessage}
+          name={name}
           {...variantProps}
           {...props}
           {...overrideProps}
