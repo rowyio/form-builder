19755a288a42053cc634bae7e7a19c2aae3f8cf7
Sidney Alcantara <sidney@sidney.me>
2021-03-15 16:43:48 +1100

    attempt next.js fixes

--- example/index.tsx
+++ example/index.tsx
@@ -57,6 +57,11 @@ const App = () => {
             label: 'Tell me more',
           },
           {
+            type: FIELDS.richText,
+            name: 'richText',
+            label: 'Tell me more, but with rich text',
+          },
+          {
             type: FIELDS.singleSelect,
             name: 'singleSelect',
             label: 'Who is the CEO of Antler?',
